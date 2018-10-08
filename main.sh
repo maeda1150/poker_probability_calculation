074c68786b5899de1c9a006b1349b7abc8c9b1d7
maeda1150 <masaki19770108@gmail.com>
2018-10-08 11:58:27 +0900

    show pre flop
    modified:   main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo '## Pre-Flop'
 ruby main.rb 1s2h 100000
 echo
 ruby main.rb 1s3h 100000
@@ -45,3 +46,12 @@ echo
 ruby main.rb 8s8h 100000
 echo
 ruby main.rb 13s13h 100000
+echo
+echo '## Flop'
+ruby main.rb 8s9h7s10d2h 100000
+echo
+ruby main.rb 1s8s12s5s10h 100000
+echo
+ruby main.rb 12s11d2c5s6h 100000
+echo
+ruby main.rb 7s7d2c10s13h 100000
