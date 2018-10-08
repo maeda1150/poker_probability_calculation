6d6d64dcd38b2c04dfc21625ad17a38adb7b5f72
maeda1150 <masaki19770108@gmail.com>
2018-10-08 12:00:28 +0900

    add flop.sh & pre_flop.sh

---
+++ flop.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo '## Flop'
+ruby main.rb 8s9h7s10d2h 100000
+echo
+ruby main.rb 1s8s12s5s10h 100000
+echo
+ruby main.rb 12s11d2c5s6h 100000
+echo
+ruby main.rb 7s7d2c10s13h 100000
---
+++ pre_flop.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+echo '## Pre-Flop'
+ruby main.rb 1s2h 100000
+echo
+ruby main.rb 1s3h 100000
+echo
+ruby main.rb 1s4h 100000
+echo
+ruby main.rb 1s8h 100000
+echo
+ruby main.rb 1s13h 100000
+echo
+ruby main.rb 3s4h 100000
+echo
+ruby main.rb 3s5h 100000
+echo
+ruby main.rb 3s6h 100000
+echo
+ruby main.rb 3s7h 100000
+echo
+ruby main.rb 3s8h 100000
+echo
+ruby main.rb 3s9h 100000
+echo
+ruby main.rb 7s8h 100000
+echo
+ruby main.rb 7s9h 100000
+echo
+ruby main.rb 7s10h 100000
+echo
+ruby main.rb 7s11h 100000
+echo
+ruby main.rb 12s13h 100000
+echo
+ruby main.rb 1s2s 100000
+echo
+ruby main.rb 1s13s 100000
+echo
+ruby main.rb 12s13s 100000
+echo
+ruby main.rb 9s10s 100000
+echo
+ruby main.rb 1s1h 100000
+echo
+ruby main.rb 8s8h 100000
+echo
+ruby main.rb 13s13h 100000
